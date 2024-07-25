d34d61717dd36ed451f3e23fc34c051d36ddfe43
yonatan957 <yonatan22660@gmail.com>
2024-07-25 17:17:43 +0300

    Add project files.

---
+++ Cars/Controllers/Car.cs
@@ -0,0 +1,24 @@
+﻿using Cars.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Controllers
+{
+    public class Car : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Model(string year, string name, string color)
+        {
+            CarModel model = new CarModel()
+            {
+                name = name ?? "error",
+                year = year ?? "error",
+                color = color ?? "white"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Cars/Models/CarModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cars.Models
+{
+    public class CarModel
+    {
+        public string year { get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+}
